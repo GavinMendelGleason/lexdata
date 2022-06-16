2ef0b3e770e4099520fce3e4af0bae7184f56a5c
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-06-16 17:12:46 +0200

    Clean up constants, remove unused functions

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::num;
 use core::mem::MaybeUninit;
 use gmp_mpfr_sys::gmp;
 use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
+use std::ffi::CStr;
+use std::os::raw::c_char;
 
-/// chain_order_encode takes a vector representing the
+/// size_encode takes a vector representing the
 /// order of a number and converts it into a vector of bytes
 ///
 /// Format of the first byte is:
@@ -22,32 +24,19 @@ use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
 /// The first bit is a sign bit, we will twos complement the
 /// entire result and therby end up with a negative, so it
 /// is always 1 here.
-///fn chain_order_encode(v : Vec<u8>) -> Vec<u8> {
-///}
 
-fn bytes_and_pad(bits : usize) -> (usize,u8) {
-    assert!(bits > 0);
-    let mut bytes = 0;
-    let mut remainder = bits as i64;
-    let mut shift = 2;
-    while remainder > 0 {
-        remainder = remainder - (8 - shift);
-        bytes += 1;
-        shift = 1;
-    }
-    (bytes, remainder.abs() as u8)
-}
-
-fn make_ones_mask(size : u8) -> u8 {
-    2 ^ size - 1
-}
 const TERMINAL : usize = 0;
+const FIRST_SIGN : usize = 0b10000000usize;
+const FIRST_TERMINAL : usize = 0b00000000usize;
 const CONTINUATION : usize = 0b10000000usize;
 const FIRST_CONTINUATION : usize = 0b01000000usize;
 const BASE_MASK : usize = !CONTINUATION;
-const FIRST_MASK : usize = !FIRST_CONTINUATION;
+const FIRST_MASK : usize = !(FIRST_SIGN | FIRST_CONTINUATION);
+const FIRST_MAX : usize = FIRST_CONTINUATION;
 
 fn size_enc(size : usize) -> Vec<u8> {
+    println!("size_enc");
+    println!("-------------------------------");
     let mut remainder = size;
     let mut v = vec![];
     let mut last = true;
@@ -57,27 +46,27 @@ fn size_enc(size : usize) -> Vec<u8> {
             println!("big");
             let continued = if last {TERMINAL} else {CONTINUATION};
             println!("continued: {:#b}", continued);
-            let byte = ((remainder & BASE_MASK) | continued) as u8;
+            let byte = (continued | (remainder & BASE_MASK)) as u8;
             println!("byte: {:#b}", byte);
             v.push(byte);
-        }else if remainder >= FIRST_CONTINUATION {
+        }else if remainder >= FIRST_MAX {
             // special case where we fit in 7 bits but not 6
             // and we need a zero padded initial byte.
             println!("medium");
-            let continued = if last {0} else {CONTINUATION};
+            let continued = if last {TERMINAL} else {CONTINUATION};
             println!("continued: {:#b}", continued);
-            let byte = ((remainder & BASE_MASK) | continued) as u8;
+            let byte = (continued | (remainder & BASE_MASK)) as u8;
             println!("byte: {:#b}", byte);
             v.push(byte);
             println!("zero pad");
-            let byte = FIRST_CONTINUATION as u8;
+            let byte = (FIRST_SIGN | FIRST_CONTINUATION) as u8;
             println!("byte: {:#b}", byte);
             v.push(byte)
         }else{
             println!("small");
-            let continued = if last {0} else {FIRST_CONTINUATION};
+            let continued = if last {FIRST_TERMINAL} else {FIRST_CONTINUATION};
             println!("continued: {:#b}", continued);
-            let byte = ((remainder & FIRST_MASK) | continued) as u8;
+            let byte = (FIRST_SIGN | continued | (remainder & FIRST_MASK)) as u8;
             println!("byte: {:#b}", byte);
             v.push(byte)
         }
@@ -88,36 +77,42 @@ fn size_enc(size : usize) -> Vec<u8> {
     v
 }
 
-fn size_dec(v : &[u8]) -> usize {
+fn size_dec(v : &[u8]) -> (usize,usize) {
+    println!("size_dec");
+    println!("-------------------------------");
     println!("v: {:?}", v);
     let mut size = 0;
     for i in 0..v.len() {
-        println!("size: {:}",size);
         let vi = v[i] as usize;
         println!("v[i]: {:#b}", v[i]);
         if i == 0 {
             if vi & FIRST_CONTINUATION == 0 {
-                return size & FIRST_MASK
+                return (vi & FIRST_MASK,i+1)
             }else{
                 size = size + (vi & FIRST_MASK)
             }
         }else{
             if vi & CONTINUATION == 0 {
-                println!("continuation: {:#b}", CONTINUATION);
-                return size + (vi & BASE_MASK)
+                return (size + (vi & BASE_MASK),i+1)
             }else{
                 size = size + (vi & BASE_MASK)
             }
         }
         size = size << 7;
     }
-    size
+    (size,v.len())
 }
 
 fn limb_vec(l : gmp_mpfr_sys::gmp::limb_t) -> Vec<u8> {
     let mut wtr = vec![];
     wtr.write_u64::<BigEndian>(l).unwrap();
     wtr
+}
+
+fn negate(v : &mut [u8]) -> () {
+    for i in 0..v.len() {
+        v[i] = !v[i]
+    }
 }
 
 // We need to panic if this is wrong
@@ -138,24 +133,37 @@ fn convert_mpz_lex(z : *mut gmp_mpfr_sys::gmp::mpz_t) -> Vec<u8> {
         println!("size: {:}", size);
         println!("zero_bytes: {:}", zero_bytes);
         let bytes = size * BYTES_PER_WORD - zero_bytes;
-        let encoded_size = size_enc(bytes);
-        let mut vec = limb_vec(limb0)[zero_bytes..BYTES_PER_WORD].to_vec();
+        let mut vec = size_enc(bytes);
+        println!("encoded size: {:?}", vec);
+        let mut limb_vector = limb_vec(limb0)[zero_bytes..BYTES_PER_WORD].to_vec();
         for i in 1..size {
             let limb_num = unsafe{ gmp::mpz_getlimbn(z,i as i64) };
             let mut limb = limb_vec(limb_num);
-            vec.append(&mut limb);
+            limb_vector.append(&mut limb);
         }
+        vec.append(&mut limb_vector);
         if sign == -1 {
-            negate(&mut vec)
+            negate(&mut vec);
         }
-        println!("{:?}", vec);
+        println!("vector: {:?}", vec);
         return vec
     }
 }
 
-fn negate(v : &mut [u8]) -> () {
-    for i in 0..v.len() {
-        v[i] = !v[i]
+// we have to pass in the pointer since we don't know anything about lifetimes.
+fn convert_lex_mpz(v : &[u8], z : *mut gmp_mpfr_sys::gmp::mpz_t) -> () {
+    let (size,offset) = size_dec(v);
+    println!("size: {:}, offset: {:}",size,offset);
+    for i in offset..size+1 {
+        if i != offset {
+            unsafe{
+                gmp::mpz_pow_ui(z,z,2^7);
+            }
+        }
+        println!("v[i] = {:}", v[i]);
+        unsafe{
+            gmp::mpz_add_ui(z,z,v[i] as u64);
+        }
     }
 }
 
@@ -168,16 +176,17 @@ mod tests {
 
     use core::mem::MaybeUninit;
     use gmp_mpfr_sys::gmp;
+    use std::ffi::{CStr,CString};
 
     #[test]
     fn find_bytes_and_pad() {
         let size = 4095; // [0b00001111u8,0b11111111u8];
         let enc = crate::size_enc(size);
-        assert_eq!(enc, vec![0b01011111u8, 0b01111111u8]);
+        assert_eq!(enc, vec![0b11011111u8, 0b01111111u8]);
 
         let size = 72057594037927935;
         let enc = crate::size_enc(size);
-        assert_eq!(enc, vec![0b01000000u8, 0b11111111u8, 0b11111111u8,
+        assert_eq!(enc, vec![0b11000000u8, 0b11111111u8, 0b11111111u8,
                              0b11111111u8, 0b11111111u8, 0b11111111u8,
                              0b11111111u8, 0b11111111u8, 0b01111111u8 ])
 
@@ -186,25 +195,70 @@ mod tests {
     #[test]
     fn in_and_out() {
         let size = 4095; // [0b00001111u8,0b11111111u8];
-        assert_eq!(size, crate::size_dec(&crate::size_enc(size)));
+        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).0);
 
         // just a random number
         let size = 23423423;
-        assert_eq!(size, crate::size_dec(&crate::size_enc(size)));
+        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).0);
 
         // boundary case for overflow spillover
         let size = 72057594037927935;
-        assert_eq!(size, crate::size_dec(&crate::size_enc(size)));
+        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).0);
 
+        let size = 1;
+        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).0);
     }
 
     #[test]
     fn mpz_lex_conversions() {
         unsafe {
-            let mut z = MaybeUninit::uninit();
-            gmp::mpz_init(z.as_mut_ptr());
-            let mut z = z.assume_init();
-            gmp::mpz_set_ui(&mut z, 15);
+            let mut z1 = MaybeUninit::uninit();
+            gmp::mpz_init(z1.as_mut_ptr());
+            let mut z1 = z1.assume_init();
+            let s1 = "1";
+            let cstring1 = CString::new(s1).expect("Cstring::new failed");
+            let cstring_ptr1 = cstring1.as_ptr();
+            gmp::mpz_init_set_str(&mut z1, cstring_ptr1, 10);
+            let mut z2 = MaybeUninit::uninit();
+            gmp::mpz_init(z2.as_mut_ptr());
+            let mut z2 = z2.assume_init();
+
+            let u = gmp::mpz_get_ui(&z2);
+            assert_eq!(u,1);
+
+            crate::convert_lex_mpz(&crate::convert_mpz_lex(&mut z1),&mut z2);
+            let cstring_ptr2 : *mut i8 = std::ptr::null_mut();
+            gmp::mpz_get_str(cstring_ptr2, 10, &mut z2);
+
+            let s2 = CStr::from_ptr(cstring_ptr2).to_string_lossy().into_owned();
+            assert_eq!(s1,"asdf");
+            assert_eq!(s1,s2)
+
+        }
+
+        unsafe {
+            let mut z1 = MaybeUninit::uninit();
+            gmp::mpz_init(z1.as_mut_ptr());
+            let mut z1 = z1.assume_init();
+            let s1 = "23423498723432";
+            let cstring1 = CString::new(s1).expect("Cstring::new failed");
+            let cstring_ptr1 = cstring1.as_ptr();
+            gmp::mpz_init_set_str(&mut z1, cstring_ptr1, 10);
+            let mut z2 = MaybeUninit::uninit();
+            gmp::mpz_init(z2.as_mut_ptr());
+            let mut z2 = z2.assume_init();
+
+            crate::convert_lex_mpz(&crate::convert_mpz_lex(&mut z1),&mut z2);
+
+            let cstring_ptr2 : *mut i8 = std::ptr::null_mut();
+            gmp::mpz_get_str(cstring_ptr2, 10, &mut z2);
+            assert_eq!(1,2);
+
+            let s2 = CStr::from_ptr(cstring_ptr2).to_string_lossy().into_owned();
+            
+            assert_eq!(s1,"asdf");
+            assert_eq!(s1,s2)
+
         }
     }
 
@@ -216,8 +270,18 @@ mod tests {
             let mut z = z.assume_init();
             gmp::mpz_set_si(&mut z, 4095);
             let vec = crate::convert_mpz_lex(&mut z);
-            assert_eq!(vec, vec![0]);
+            assert_eq!(vec, vec![130, 15, 255]);
         }
+
+        unsafe {
+            let mut z = MaybeUninit::uninit();
+            gmp::mpz_init(z.as_mut_ptr());
+            let mut z = z.assume_init();
+            gmp::mpz_set_si(&mut z, -4095);
+            let vec = crate::convert_mpz_lex(&mut z);
+            assert_eq!(vec, vec![125, 240, 0]);
+        }
+
     }
 
     #[test]
